dc6a53fa3a2af6df5508401c9ca5bab066bee884
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-11-06 10:08:47 +0000

    Add new class task00 with boilerplate code

---
+++ Task00.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NcCsTypes
+{
+    internal class Task00
+    {
+        public static void Run()
+        {
+            Console.WriteLine("This works!");
+        }
+
+    }
+}
